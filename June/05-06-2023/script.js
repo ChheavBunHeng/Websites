e9a4a873cc308b5d42453e7640e52d8b5283b924
DuckQuack <64895072+ChheavBunHeng@users.noreply.github.com>
2023-06-05 23:02:52 +0700

    Add files via upload

---
+++ June/05-06-2023/script.js
@@ -0,0 +1,16 @@
+// const mysql = require('mysql2');
+const Typed = require("typed.js");
+const welcome = document.querySelector('#welcome');
+
+// const connection = mysql.connection({
+//     host:'localhost',
+//     user:'root',
+//     password:'1234',
+//     database:'school'
+// });
+
+
+// const typed = new Typed(welcome,{
+//     strings:['Welcome Adventurer'],
+//     typeSpeed:100
+// });
