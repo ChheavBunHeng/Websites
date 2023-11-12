6a8000483fb5ad14865cc0cb056f0ef54f979283
ChheavBunHeng <bunchhengchheav@gmail.com>
2023-11-12 15:24:08 +0700

    Testing With some compnonment

---
+++ November/12-Nov-2023-Wrong/script.js
@@ -0,0 +1,10 @@
+const fandI = (url, targetClass) => {
+    fetch(url)
+        .then(Response => Response.text())
+        .then(data => document.querySelector(`.${targetClass}`).innerHTML = data)
+        .catch(error => console.error("Error the fetch the data",error))
+};
+
+document.addEventListener("DOMContentLoaded",(){
+    fandI("./header.html","header");
+});
