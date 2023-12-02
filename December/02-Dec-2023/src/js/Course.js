44ab057dc4e99e6d807b4356731f15b96a9cb059
ChheavBunHeng <bunchhengchheav@gmail.com>
2023-12-02 12:10:53 +0700

    Update and create new file

---
+++ December/02-Dec-2023/src/js/Course.js
@@ -0,0 +1,13 @@
+const accordion = document.querySelectorAll(".accordion");
+
+for (var i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
