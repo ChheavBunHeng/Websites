395bce7ba58dc846aceea830b13f53bbf5320c4c
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-01-11 19:25:10 +0700

    Updating the code

--- 2023/December/31-Dec-2023/src/reservations.js
+++ 2023/December/31-Dec-2023/src/reservations.js
@@ -1,4 +1,22 @@
-const RegisterBTN = document.querySelector(".RegisterBTN");
-RegisterBTN.addEventListener("click",()=>{
-    console.log("Hello world"); 
+const RegisterBTN = document.querySelector(".RegisterBTN"); 
+
+RegisterBTN.addEventListener("click", () => {
+    const jsonData = {
+        Date: document.querySelector(".reservation-day").value,
+        Time: document.querySelector(".reservation-time").value,
+        Number_of_people: document.querySelector(".number-of-guest").value,
+        PhoneNumber: document.querySelector(".phone").value,
+        Email: document.querySelector(".email").value,
+        SpecialRequest: document.querySelector(".special-requests").value    
+    };
+    var json = JSON.stringify(jsonData);
+    console.log(json);
+    ClearInput();
 });
+
+function ClearInput()
+{
+    document.querySelectorAll(".reservations-form input").forEach(function(element) {
+        element.value = "";
+    });   
+}   
