4df03aaa3165f786f7fe22023cbfec3a524005ca
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-01-27 01:26:03 +0700

    first react app

---
+++ 2024/Janauray/27-Jan-2024/my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://reactjs.org">Hello world</a>
+    </div>
+  );
+}
+
+export default App;
