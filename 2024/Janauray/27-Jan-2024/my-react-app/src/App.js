9e0a5228fccc6d2d51cbbbc5994a6f1ea21b1eb1
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-01-27 11:00:12 +0700

    first react app

--- 2024/Janauray/27-Jan-2024/my-react-app/src/App.js
+++ 2024/Janauray/27-Jan-2024/my-react-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <a href="https://reactjs.org">Hello world</a>
     </div>
   );
---
+++ 2024/Janauray/27-Jan-2024/my-react-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="Header">
+            <a href="#">Hello world</a>
+            <a href="#">Hello world</a>
+            <a href="#">Hello world</a>
+        </div>
+    );
+};
+
+export default Navbar;
