b9839ddac3dcbe82fddda24e2dccee8785c5f30d
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-02-10 14:22:20 +0700

    React

--- 2024/Febuary/09-Feb-2024/week1/src/components/Signup.js
+++ 2024/Febuary/09-Feb-2024/week1/src/components/Signup.js
@@ -4,13 +4,18 @@ import "../css/Signup.css";
 const Signup = () => {
   return (
     <div className="Signup">
+      <div className="UserName">
         <input type="text" className="UserNameKh" placeholder="Input khmer name" />
         <input type="text" className="UserNameEnglish" placeholder="Input english name" />
+      </div>
+      <div>
+
+        <input type="text" className="Nationality" placeholder="Input nationality"/>
         <select name="" id="">
             <option value="Male">Male</option>  
             <option value="Female">Female</option>
         </select>
-        <input type="text" className="Nationality" placeholder="Input nationality"/>
+      </div>
         <input type="datetime" name="DateOfBirth"/> 
         <input type="text" className="Job" placeholder="Input job"/>    
         <input type="text" className="Address" placeholder="Input address"/>    
