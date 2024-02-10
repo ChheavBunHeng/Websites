1a57f40aaf90867b3001c6d6e99e5a54c4406ded
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-02-10 14:41:19 +0700

    React

--- 2024/Janauray/27-Jan-2024/my-react-app/src/components/Footer.js
+++ 2024/Janauray/27-Jan-2024/my-react-app/src/components/Footer.js
@@ -1,46 +1,29 @@
 import React from 'react';
-// import '../css/Footer.css'; // Replace 'Footer.css' with the actual name of your CSS file
 import '../css/Footer.css'; // Replace 'Footer.css' with the actual name of your CSS file
-// import { Link }
+import Qrcode from "react-qr-code";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="QRcode">
         <div className="detail">
-          <h1>QR Code</h1>
-          <img src="" alt="Cito Qr code" />
+          <h1>More Info</h1>
+          <Qrcode value='https://web.facebook.com/citocomputers'></Qrcode>
         </div>
-      </div>
       <div className="SocialMedia">
         <div className="detail">
           <h1>Social Media</h1>
-          <i className="fab fa-facebook">
-            <a href="https://web.facebook.com/citocomputers">Facebook</a>
-          </i>
-          <i className="fab fa-telegram">
-            <a href="https://t.me/citocomputer">Telegram</a>
-          </i>
-          <i className="fab fa-youtube">
-            <a href="https://www.youtube.com/@citocomputer">Youtube</a>
-          </i>
+          <a href="https://web.facebook.com/citocomputers">Facebook</a>
+          <a href="https://t.me/citocomputer">Telegram</a>
+          <a href="https://www.youtube.com/@citocomputer">Youtube</a>
         </div>
       </div>
       <div className="Contact_info">
         <div className="detail">
           <h1>Contact Info</h1>
-          <i className="fas fa-phone-alt">
-            <a href="#">(+855) 099 999 999</a>
-          </i>
-          <i className="fas fa-phone-alt">
-            <a href="#">(+855) 099 999 999</a>
-          </i>
-          <i className="fas fa-envelope">
-            <a href="#">Email</a>
-          </i>
-          {/* <i className="fas fa-map-marker-alt">
-            <a href="#">Address</a>
-          </i> */}
+          <a href="#">(+855) 099 999 999</a>
+          <a href="#">(+855) 099 999 999</a>
+          <a href="#">Email</a>
+          {/* <a href="#">Address</a> */}
         </div>
       </div>
     </div>
