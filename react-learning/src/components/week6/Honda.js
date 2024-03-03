bd617bb76e6cf47c82fe3568645db69cdfc6dbe9
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-03-04 00:39:26 +0700

    Test

--- react-learning/src/components/week6/Honda.js
+++ react-learning/src/components/week6/Honda.js
@@ -5,7 +5,7 @@ import "./Honda.css";
 const Honda = () => {
     return (
         <div class="flex bg-blue-300 justify-around flex-col fa-align-center">
-            <h1 className="Name">Honda</h1>
+            <h1 class="flex">Honda</h1>
             <h3>Hello world</h3>
         </div>
     );
