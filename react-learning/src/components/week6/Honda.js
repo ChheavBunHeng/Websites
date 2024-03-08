20114eb99d1e5d2dd9022a329d30bb2f1eebe7d4
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-03-09 01:15:20 +0700

    Test tailwind 1

--- react-learning/src/components/week6/Honda.js
+++ react-learning/src/components/week6/Honda.js
@@ -3,10 +3,15 @@ import React from "react";
 import "./Honda.css";
 
 const Honda = () => {
-    return (
-        <div class="flex bg-blue-300 justify-around flex-col fa-align-center">
-            <h1 class="flex">Honda</h1>
-            <h3>Hello world</h3>
+    return (    
+        <div>
+            <div className="flex bg-blue-300 justify-around flex-col fa-align-center">
+                <h1 className="font-bold">Honda</h1>
+                <h3>Hello world</h3>
+            </div>
+            <div className="flex justify-center fa-align-center">
+                <h1 className="text-3xl font-bold text-red-950">Hello world</h1>
+            </div>
         </div>
     );
 };
