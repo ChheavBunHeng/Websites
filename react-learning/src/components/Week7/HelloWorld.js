ed9d8084e9e1399c5bcf9e53cb0bfa6180be59ff
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-03-10 14:54:34 +0700

    Week 7

--- react-learning/src/components/Week7/HelloWorld.js
+++ react-learning/src/components/Week7/HelloWorld.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./HelloWorld.css";
 import Navbar from "./Navbar";
-import Login from "./LoginPage.js";
 import LoginPage from "./LoginPage.js";
-const HelloWorld = () =>{
-    return(
-        <Navbar></Navbar>
-        <LoginPage></LoginPage>
 
-    );
+const HelloWorld = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="content">
+      <LoginPage />
+      </div>
+    </>
+  );
 };
+
 export default HelloWorld;
