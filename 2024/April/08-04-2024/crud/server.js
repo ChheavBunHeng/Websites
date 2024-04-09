2a238f6ae28775beaaa22e096e87360f9cf54ba3
DuckQuack <bunchhengchheav@gmail.com>
2024-04-09 14:32:43 +0700

    Website

--- 2024/April/08-04-2024/crud/server.js
+++ 2024/April/08-04-2024/crud/server.js
@@ -1,27 +1,9 @@
-const express = require('express');
-const mysql = require('mysql2');
+const express = require("express");
 
-const app = express();
+var app = express();
 
-const pool = mysql.createPool({
-    host: 'your_host',
-    user: 'root',
-    password: '1234',
-    database: 'test',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+app.get("/hello", (req,res) =>{
+    res.send("Hello World");
 });
 
-app.get('/users', (req, res) => {
-    pool.query('SELECT * FROM users', (err, rows) => {
-        if (err) {
-            return res.status(500).json({ error: 'Error fetching users' });
-        }
-        res.json(rows);
-    });
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(3000);
