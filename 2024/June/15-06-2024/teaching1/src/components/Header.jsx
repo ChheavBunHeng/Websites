3f6694aa4669c55a0380da74c03009b3b6a2c4c1
ChheavBunHeng <bunchhengchheav@gmail.com>
2024-06-17 14:30:22 +0700

    teaching kaka

---
+++ 2024/June/15-06-2024/teaching1/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        
+        <div className="flex justify-center header bold text-red-600 bg-slate-700">
+            <h1>Expense Tracker</h1>
+            <p>Track your all expenses in one place</p>
+        </div>
+    )
+}
+
+
+export default Header;
