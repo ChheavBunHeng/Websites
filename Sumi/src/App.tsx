bbe3e075ffd19e46349ee45c018cfb7a6341212f
DuckQuack <bunchhengchheav@gmail.com>
2024-09-27 00:29:28 +0700

    Proejct S

---
+++ Sumi/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import NavBar from './components/NavBar'
+import Test from "./Components/Test"
+import Section from "./Components/Section.jsx"
+import Testimonial from "./Components/Testimonial"
+function App() {
+  return(
+    <>
+      <NavBar></NavBar>
+      <Test></Test>
+      <Section></Section>
+      <Testimonial></Testimonial>
+    </>
+  )
+
+  }
+
+export default App
